568e134835e9aba99dffa61dd7ee46fe3226be78
Trishit Ghosh <trishitghosh@MacBook-Air.local>
2024-12-17 12:01:40 +0530

    Contd Project

--- src/components/Player.tsx
+++ src/components/Player.tsx
@@ -1,10 +1,56 @@
-import { View, Text, StyleSheet, Image } from "react-native";
-import React from "react";
-
+import { View, Text, StyleSheet, Image, DimensionValue } from "react-native";
+import React, { useEffect, useState } from "react";
+import { Audio, AVPlaybackStatus } from "expo-av";
+import { Sound } from "expo-av/build/Audio";
 import { Ionicons } from "@expo/vector-icons";
 import { usePlayerContext } from "../providers/PlayerProvider";
+
 const Player = () => {
   const { track } = usePlayerContext();
+  const [sound, setSound] = useState<Sound>();
+  const [isPlaying, setIsPlaying] = useState<boolean>();
+  const [trackPos, setTrackPos] = useState<DimensionValue>();
+  useEffect(() => {
+    playTrack();
+  }, [track]);
+
+  useEffect(() => {
+    return sound
+      ? () => {
+          sound.unloadAsync();
+        }
+      : undefined;
+  }, [sound]);
+  const playTrack = async () => {
+    if (!track?.preview_url) return;
+
+    const { sound: newSound } = await Audio.Sound.createAsync({
+      uri: track.preview_url,
+    });
+    setSound(newSound);
+    newSound.setOnPlaybackStatusUpdate(onPlaybackStatusUpdate);
+    await newSound.playAsync();
+  };
+
+  const PlayPauseTrack = async () => {
+    if (!sound) return;
+
+    if (isPlaying) await sound.pauseAsync();
+    else await sound.playAsync();
+  };
+
+  const onPlaybackStatusUpdate = (status: AVPlaybackStatus) => {
+    if (!status.isLoaded) return;
+    setIsPlaying(status.isPlaying);
+    if (status.durationMillis) {
+      let percent: string = (
+        (status.positionMillis / status.durationMillis) *
+        100
+      ).toFixed(0);
+      let progress: `${number}%` = `${parseInt(percent)}%`;
+      setTrackPos(progress);
+    }
+  };
   if (!track) return null;
   return (
     <View style={styles.container}>
@@ -22,10 +68,22 @@ const Player = () => {
         />
         <Ionicons
           disabled={!track?.preview_url}
-          name={"play"}
+          name={isPlaying ? "pause" : "play"}
           size={22}
           color={track?.preview_url ? "white" : "gray"}
+          onPress={PlayPauseTrack}
         />
+      </View>
+      <View
+        style={{
+          backgroundColor: "white",
+          height: 3,
+          borderRadius: 5,
+          top: -4,
+          width: trackPos,
+        }}
+      >
+        {/*Seeking Bar*/}
       </View>
     </View>
   );
@@ -44,7 +102,7 @@ const styles = StyleSheet.create({
     flexDirection: "row",
     flex: 1,
     borderRadius: 5,
-    padding: 3,
+    padding: 5,
     paddingRight: 15,
   },
 
@@ -53,6 +111,7 @@ const styles = StyleSheet.create({
     aspectRatio: 1,
     borderRadius: 5,
     marginRight: 10,
+    marginBottom: 5,
   },
   albname: {
     color: "white",
